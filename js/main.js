cafcaca6204ad9351c5917fe1b835eaabe7b9e90
fariasraphael <farias.raphael@gmail.com>
2023-05-26 21:43:23 -0300

    added task status div's id, modified taskcard to object, created taskCards array for taskCard storage, changed createNewCard function to accept object as parameter, created taskCard unique id verification, added drag property on taskCard.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,62 @@
 let form = document.getElementsByTagName("form")[0];
 let newCardButton = document.getElementsByTagName("button")[0];
+let taskCards = []
+
+let todo = document.querySelector(".todo");
+let doing = document.querySelector(".doing");
+let done = document.querySelector(".done");
 
 
 newCardButton.addEventListener("click", () => {
     form.classList.contains("d-none") ? form.classList.remove("d-none") : form.classList.add("d-none");
+    newCardButton.innerHTML === "+" ? newCardButton.innerHTML = "-" : newCardButton.innerHTML = "+"
 });
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let title = form.elements["title"].value
-    let description = form.elements["descirption"].value
+    
+    let title = form.elements['title'].value;
+    let description = form.elements['descirption'].value;
     form.classList.add("d-none");
     form.reset();
-    createNewCard(title, description);
+    
+    const taskCard = {
+        title: title,
+        description: description
+    }
+    const exist = taskCards.find(element => element.title === title);
+    
+    console.log(exist);
+
+    if (exist) {
+        taskCard.id = exist.id;
+        taskCards[taskCards.findIndex(elemento => elemento.id === exist.id)] = taskCard;
+    } else {
+        taskCard.id = taskCards[taskCards.length -1] ? (taskCards[taskCards.length -1]).id +1 : 0;
+
+        createNewCard(taskCard);
+        taskCards.push(taskCard)
+    
+        console.log(taskCard.id);
+
+    }
 });
 
-function createNewCard(title, description) {
+//function cardTaskId () {
+//    const exist = taskCards.find(elemento => elemento.title === title.value)
+
+//    let taskCards = document.querySelectorAll(".card");
+  //  divNewCard.id = "draggable-item" + taskCards.length;
+ //   if (taskCards.id === divNewCard.id) {
+  //      divNewCard.id = "draggable-item" + taskCards.length+1;
+  //  } else {
+  //      divNewCard.id = "draggable-item" + taskCards.length;
+  //  }
+//}
+
+
+function createNewCard(taskCard) {
+    
     let divNewCard = document.createElement("div");
     divNewCard.classList.add("card", "d-flex", "flex-row-reverse", "align-items-start");
     divNewCard.style.width = "16rem";
@@ -35,12 +76,15 @@ function createNewCard(title, description) {
     })
 
     let divNewCardBody = document.createElement("div");
-    divNewCardBody.classList.add("card-body")
+    divNewCardBody.classList.add("card-body");
+    divNewCard.draggable = true;
+    divNewCard.id = "draggable-item["+taskCard.id+"]";
+
 
     let titleH5 = document.createElement("h5");
-    titleH5.innerHTML = title;
+    titleH5.innerHTML = taskCard.title;
     let descriptionP = document.createElement("p");
-    descriptionP.innerHTML = description;
+    descriptionP.innerHTML = taskCard.description;
 
     divNewCardBody.appendChild(titleH5)
     divNewCardBody.appendChild(descriptionP)
@@ -48,17 +92,44 @@ function createNewCard(title, description) {
     divNewCard.appendChild(buttonDelete);
     divNewCard.appendChild(buttonEdit);
     divNewCard.appendChild(divNewCardBody)
+    
+    
 
     let body = document.getElementById("toDo");
     body.insertBefore(divNewCard, null);
+
+    dragStart(divNewCard);
+
 }
 
+
 function editTask(element) {
-    let titleEdit = element.target.parentElement. children[2].firstChild.innerHTML;
-    let descriptionEdit = element.target.parentElement. children[2].lastChild.innerHTML;
+    let titleEdit = element.target.parentElement.children[2].firstChild.innerHTML;
+    let descriptionEdit = element.target.parentElement.children[2].lastChild.innerHTML;
     console.log(titleEdit, descriptionEdit);
     form.classList.remove("d-none")
     form.elements["title"].value = titleEdit;
     form.elements["descirption"].value = descriptionEdit;
     element.target.parentElement.remove();
+
+}
+
+function dragStart(element) {
+
+    element.ondragstart = event => {
+        event.dataTransfer.setData("tarefa-id", event.target.id);
+        if (element.parentElement === todo) {
+            todo.classList.remove("avaliable-to-drop");
+            doing.classList.add("avaliable-to-drop");
+            done.classList.add("avaliable-to-drop");
+        } else if (element.parentElement === doing) {
+            doing.classList.remove("avaliable-to-drop");
+            todo.classList.add("avaliable-to-drop");
+            done.classList.add("avaliable-to-drop");
+        } else {
+            done.classList.remove("avaliable-to-drop");
+            doing.classList.add("avaliable-to-drop");
+            todo.classList.add("avaliable-to-drop");
+        }
+    }
 }
